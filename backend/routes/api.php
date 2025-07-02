4eff666e1547c0532bbea3d3ad5ff0f6e04e1bc5
Chhea012 <chhea.chhouy@student.passerellesnumeriques.org>
2025-07-02 19:15:04 +0700

    add route structure

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\Users\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Expr\FuncCall;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::prefix('v1')->group(function (){
+    Route::apiResource('/role', RoleController::class);
 });
